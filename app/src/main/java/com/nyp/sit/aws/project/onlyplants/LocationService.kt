4c95943ea82e30442fd5f7889ca1b71508099689
weijie-n <84274319+weijie-n@users.noreply.github.com>
2023-02-13 01:30:41 +0800

    updates

--- app/src/main/java/com/nyp/sit/aws/project/onlyplants/LocationService.kt
+++ app/src/main/java/com/nyp/sit/aws/project/onlyplants/LocationService.kt
@@ -2,10 +2,153 @@ package com.nyp.sit.aws.project.onlyplants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.geo.location.AWSLocationGeoPlugin
+import com.amplifyframework.geo.location.models.AmazonLocationPlace
+import com.amplifyframework.geo.maplibre.view.MapLibreView
+import com.amplifyframework.geo.maplibre.view.support.fadeIn
+import com.amplifyframework.geo.maplibre.view.support.fadeOut
+import com.amplifyframework.geo.models.Coordinates
+import com.amplifyframework.geo.options.GeoSearchByCoordinatesOptions
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapboxMap
+
+//import com.amazonaws.services.location.model.*
+//import com.amazonaws.mobileconnectors.geo.tracker.* // Required for Amazon Location Service
+//import com.amazonaws.mobileconnectors.geo.tracker.publisher.* // Required for Amazon Location Service
+//import com.amazonaws.mobileconnectors.geo.tracker.service.* // Required for Amazon Location Service
+//import com.amazonaws.services.geo.model.* // Required for Amazon Location Service
+//import com.amplifyframework.core.model.temporal.Temporal.DateTime
+//import com.amplifyframework.geo.AWSLocationProvider
+//import com.amplifyframework.geo.models.*
+//import java.util.*
+
 
 class LocationService : AppCompatActivity() {
+
+    private val mapView by lazy {
+        findViewById<MapLibreView>(R.id.mapView)
+    }
+
+    private val descriptionView by lazy {
+        findViewById<TextView>(R.id.description_text_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initAmplify()
         setContentView(R.layout.activity_location_service)
+
+        mapView.getMapAsync { map ->
+            val initialPosition = LatLng(1.3800, 103.8489)
+            map.cameraPosition = CameraPosition.Builder()
+                .target(initialPosition)
+                .zoom(13.0)
+                .build()
+            map.addOnCameraMoveStartedListener { toggleDescriptionText() }
+            map.addOnCameraIdleListener { reverseGeocode(map) }
+    }
+}
+
+//    private fun forwardGeocode(queryText: String) {
+//        val request = SearchPlaceIndexForTextRequest.builder()
+//            .text(queryText)
+//            .maxResults(1)
+//            .build()
+//
+//        Amplify.Location.searchPlaceIndexForText(request,
+//            { result ->
+//                result.places.firstOrNull()?.let { place ->
+//                    val amazonPlace = (place as AmazonLocationPlace)
+//                    val location = amazonPlace.geometry.point
+//                    runOnUiThread { toggleDescriptionText("Latitude: ${location[1]}, Longitude: ${location[0]}") }
+//                }
+//            },
+//            { exp ->
+//                Log.e("AndroidQuickStart", "Failed to geocode : $exp")
+//            }
+//        )
+//    }
+
+    private fun reverseGeocode(map: MapboxMap) {
+        val options = GeoSearchByCoordinatesOptions.builder()
+            .maxResults(1)
+            .build()
+
+        val centerCoordinates = Coordinates().apply {
+            longitude = map.cameraPosition.target.longitude
+            latitude = map.cameraPosition.target.latitude
+        }
+        Amplify.Geo.searchByCoordinates(centerCoordinates, options,
+            { result ->
+                result.places.firstOrNull()?.let { place ->
+                    val amazonPlace = (place as AmazonLocationPlace)
+                    runOnUiThread { toggleDescriptionText(amazonPlace.label) }
+                }
+            },
+            { exp ->
+                Log.e("AndroidQuickStart", "Failed to reverse geocode : $exp")
+            }
+        )
+    }
+
+    private fun toggleDescriptionText(label: String? = "") {
+        if (label.isNullOrBlank()) {
+            descriptionView.fadeOut()
+        } else {
+            descriptionView.text = label
+            descriptionView.fadeIn()
+        }
+    }
+    private fun initAmplify() {
+        try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.addPlugin(AWSLocationGeoPlugin())
+            Amplify.configure(applicationContext)
+            Log.i("AndroidQuickStart", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("AndroidQuickStart", "Could not initialize Amplify", error)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 }
---
+++ app/src/main/java/com/nyp/sit/aws/project/onlyplants/Model/Call_Wiki.kt
@@ -0,0 +1,100 @@
+package com.nyp.sit.aws.project.onlyplants.Model
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+
+import java.io.BufferedReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class Call_Wiki {
+    private val client = OkHttpClient()
+    private val protocol = "https://"
+    private val domain = "vmo0ymtfn1.execute-api.us-east-1.amazonaws.com"
+    private val getPlantInfoPath = "/dev/plants/information"
+    private val getPlantTypePath = "/dev/plants/type"
+    private val getTranslationPath = "/translate"
+    //private val parameters = "?action=query&format=json&list=search&utf8=1&formatversion=2&srsearch="
+    //private val postfix = "%20plant"
+
+
+    /** Currently using mock API endpoints, will change later **/
+
+    //suspend fun GetWikiInformation(wiki_name : String) : Deferred<String> = CoroutineScope(IO).async{
+    suspend fun GetWikiInformation() : String{
+        //val uriPath = "$protocol$domain$getPlantInfoPath?name=$plant_name"
+        //val uriPath = "https://p8upla1755.execute-api.us-east-1.amazonaws.com/prod/testing"
+        val uriPath = "https://0vquhvi392.execute-api.ap-southeast-1.amazonaws.com/prod"
+        val request = Request.Builder().url(uriPath).build()
+
+        var GetWikiInfo = ""
+
+        Log.d("Getting Wiki information from", uriPath)
+
+        val response = client.newCall(request).execute()
+        if (response.isSuccessful){
+            GetWikiInfo = response.body.string()
+            Log.d("WikiInfoReply", GetWikiInfo)
+        }
+        else{
+            Log.d("WikiInfoRetrievalError",response.message)
+            GetWikiInfo = "Retreieved Error"
+        }
+
+        return GetWikiInfo
+    }
+    fun postWikiSearch(search:String): String {
+        val url = URL("https://0vquhvi392.execute-api.ap-southeast-1.amazonaws.com/prod/post")
+        val conn = url.openConnection() as HttpURLConnection
+        conn.requestMethod = "POST"
+        conn.doOutput = true
+
+        val json = JSONObject()
+        json.put("input",search)
+//        val test = json.getString("body")
+//        Log.d("test log",test)
+        val body = json.toString().toByteArray(Charsets.UTF_8)
+        conn.setRequestProperty("Content-Length", body.size.toString())
+        conn.setRequestProperty("Content-Type", "application/json")
+        conn.outputStream.write(body)
+        conn.connect()
+
+        val responseCode = conn.responseCode
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            val responseData = conn.inputStream.bufferedReader().use(BufferedReader::readText)
+            val jsonResponse = JSONObject(responseData)
+            val body = jsonResponse.getString("body")
+            Log.d("Post Success", body)
+            return body
+        } else {
+             Log.d("Error", responseCode.toString())
+            return responseCode.toString()
+        }
+
+    }
+
+//    suspend fun GetPlantType(plant_image_in_bytes : ByteArray) : Deferred<String> = CoroutineScope(IO).async {
+//
+//        val uriPath = "$protocol$domain$getPlantTypePath?image_bytes=$plant_image_in_bytes"
+//        val request = Request.Builder().url(uriPath).build()
+//
+//        var plantType = ""
+//
+//        Log.d("Getting plant type from", uriPath)
+//
+//        client.newCall(request).enqueue(object : Callback {
+//            override fun onResponse(call: Call, response: Response) {
+//                plantType = JSONObject(response.body.string()).getString("message").toString()
+//                Log.d("PlantTypeReply", response.body.string())
+//            }
+//
+//            override fun onFailure(call: Call, e: IOException) {
+//                //TODO("Not yet implemented")
+//                Log.d("PlantTypeRetrievalError", e.toString())
+//            }
+//        })
+//
+//        return@async plantType
+//    }
+}
